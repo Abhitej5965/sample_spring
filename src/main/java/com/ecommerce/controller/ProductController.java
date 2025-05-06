4de23d809e7b56b6ebfa8849e4f35ae4c48bae67
Abhitej25 <abhitej.konkimalla@wavemaker.com>
2025-05-06 16:46:28 +0530

    can you revoke the chnages in product controller to not to use object mapper

--- src/main/java/com/ecommerce/controller/ProductController.java
+++ src/main/java/com/ecommerce/controller/ProductController.java
@@ -2,7 +2,6 @@ package com.ecommerce.controller;
 
 import com.ecommerce.model.Product;
 import com.ecommerce.service.ProductService;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -18,9 +17,6 @@ public class ProductController {
 
     @Autowired
     private ProductService productService;
-    
-    @Autowired
-    private ObjectMapper objectMapper;
 
     @GetMapping
     public List<Product> getAllProducts() {
@@ -36,10 +32,9 @@ public class ProductController {
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<Product> createProduct(
-            @RequestPart("product") String productJson,
+            @RequestPart("product") Product product,
             @RequestPart(value = "file", required = false) MultipartFile file) {
         try {
-            Product product = objectMapper.readValue(productJson, Product.class);
             Product newProduct = productService.saveProduct(product, file);
             return ResponseEntity.ok(newProduct);
         } catch (IOException e) {
@@ -50,10 +45,9 @@ public class ProductController {
     @PutMapping(value = "/{id}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<Product> updateProduct(
             @PathVariable Long id,
-            @RequestPart("product") String productJson,
+            @RequestPart("product") Product product,
             @RequestPart(value = "file", required = false) MultipartFile file) {
         try {
-            Product product = objectMapper.readValue(productJson, Product.class);
             Product updatedProduct = productService.updateProduct(id, product, file);
             return ResponseEntity.ok(updatedProduct);
         } catch (RuntimeException | IOException e) {
